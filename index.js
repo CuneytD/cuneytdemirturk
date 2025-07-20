a98998f2cf02d886548d96930ffce45cc497c60a
CuneytD <cuneytdemirturk@gmail.com>
2025-07-20 15:38:48 +0300

    First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+//routes
+
+app.use("/products/:id", function (req, res) {
+    res.render("urun-details");    
+});
+
+app.use("/products", function (req, res) {
+    res.render("urunler");
+});
+
+app.use("/", function (req, res) {
+    res.render("index");
+});
+
+
+app.listen(3000, () => {
+    console.log("Listining  on port 3000");
+});
